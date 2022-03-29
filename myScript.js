31378f2ce77af4ae649d9bd3bf57f6879b0d5115
Kkatakuri24 <40903973+Kkatakuri24@users.noreply.github.com>
2022-03-28 23:21:07 -0500

    chart added

--- myScript.js
+++ myScript.js
@@ -33,11 +33,48 @@ checkSymbol = () => {
         if(this.readyState == 4 && this.status == 200){
             var myData = JSON.parse(this.responseText);
             displayData(myData);
+            displayChart(myData);
         }
     };
     xhttp.open("GET", url + "?" + params, true);
     xhttp.send();
     return xhttp;
+}
+
+
+displayChart = (myData) =>{
+
+    const ctx = document.getElementById('myChart').getContext('2d');
+    const myChart = new Chart(ctx, {
+    type: 'pie',
+    data: {
+        labels: ['Buy', 'Sell', 'Hold'],
+        datasets: [{
+            label: '# of professional analysis',
+            data: [myData.Buy, myData.Sell, myData.Hold],
+            backgroundColor: [
+                'rgba(22, 240, 145, 0.8)',
+                'rgba(255, 99, 132, 0.2)',
+                'rgba(54, 162, 235, 0.2)',
+                
+            ],
+            borderColor: [
+                'rgba(22, 240, 145, 1)',
+                'rgba(255, 99, 132, 1)',
+                'rgba(54, 162, 235, 1)',
+            ],
+            borderWidth: 1
+        }]
+    },
+    options: {
+        responsive: true,
+        scales: {
+            y: {
+                beginAtZero: true
+            }
+        }
+    }
+});
 }
 
 displayData = (myData) => {
@@ -49,9 +86,11 @@ displayData = (myData) => {
     display = display + "Shares Outstanding: " + myData.os + '<br>' + '<br>';
     display = display + "Float Shares: " + myData.floatS + '<br>' + '<br>';
     display = display + "FiftyTwoWeekHigh: $" + myData.fftHigh + '<br>'+ '<br>';
-    display = display + "FiftyTwoWeekLwo: $" + myData.fftLow + '<br>' + '<br>';
+    display = display + "FiftyTwoWeekLwo: $" + myData.fftLow+ '<br>' + '<br>';
+    display = display + "<div class='chart-container' style='position: relative; height:500px; width:500px'><canvas id='myChart'></canvas></div>"
     document.getElementById("displayData").innerHTML = display;
 }
+
 
 resetSymbol = () => {
     document.getElementById("ticker").value = "";
@@ -81,3 +120,4 @@ stickyNav = () => {
         nav.classList.remove("sticky");
     }
 }
+
--- scraper.py
+++ scraper.py
@@ -1,5 +1,6 @@
 import yfinance as yf
 import json
+import collections
 
 # ticker = yf.Ticker("GME")
 # info = ticker.info
@@ -23,6 +24,17 @@ class scraper:
     def search(self, tickerName):
         ticker = yf.Ticker(tickerName)
         info = ticker.info
+        holders = ticker.get_major_holders
+        hist = ticker.history(period="max")
+        recom = ticker.recommendations
+        analysis = ticker.analysis
+        for column in recom[['To Grade']]:
+            columnSeriesObj = recom[column]
+            c = collections.Counter(columnSeriesObj.values)
+            buy = c['Buy']
+            sell = c['Sell']
+            hold = c['Hold']
+
         dataDict = {}
         dataDict["lbs"] = info["longBusinessSummary"]
         dataDict["currentPrice"] = info["currentPrice"]
@@ -32,5 +44,9 @@ class scraper:
         dataDict["floatS"] = info["floatShares"]
         dataDict["fftHigh"] = info["fiftyTwoWeekHigh"]
         dataDict["fftLow"] = info["fiftyTwoWeekLow"]
+        dataDict["Buy"] = buy
+        dataDict["Sell"] = sell
+        dataDict["Hold"] = hold
         json_object = json.dumps(dataDict, indent=8)
+        print(buy, sell, hold)
         return json_object
